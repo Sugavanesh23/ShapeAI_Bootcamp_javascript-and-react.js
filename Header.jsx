7c0a88f8cc112a5af60dd0d216646920cb8ac1b4
Sugavanesh <85359586+Sugavanesh23@users.noreply.github.com>
2021-06-05 15:53:36 +0530

    Add files via upload

---
+++ Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Header from "./components/Header";
+
+function Header(){
+    return (
+      <header>
+        <h1>ShapeAI</h1>  
+      </header> 
+    );
+}
+export default Header;
